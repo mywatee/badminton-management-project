f2a2d893a12289e48b5f0a11566f46ca8f5e23eb
mywatee <srqkendra961@gmail.com>
2026-03-16 11:29:29 +0700

    1

---
+++ BadmintonManagement/BadmintonManagement.GUI/Views/BookingView.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+using QLSANCL.BadmintonManagement.ViewModels;
+
+namespace QLSANCL.BadmintonManagement.Views
+{
+    public partial class BookingView : Page
+    {
+        public BookingView()
+        {
+            InitializeComponent();
+            DataContext = new BookingViewModel();
+        }
+    }
+}
